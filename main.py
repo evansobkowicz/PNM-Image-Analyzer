dc5cbdda23a673bd33951fcf2b4ea07d696b9ea6
Evan Sobkowicz <evansobkowicz@gmail.com>
2015-02-25 10:19:04 -0500

    stubs

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 # Project 1
 # Evan Sobkowicz
+
+
+class Edger:
+
+    # Initialize Object
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+
+    # Read in pnm P2 or P3 files from text
+    def create_image(self, file_path):
+        return None
+
+    # Grayscale
+    def grayscale(self, pic):
+        return None
+
+
+    # Analyze Edges (distance of color value calculations)
+    def analyze_edges(self, pic):
+        return None
+
+
+    # Display edged image
+    def display_image(self, pic):
+        return None
+
+
+    # Save your edge image to a file using the greyscale .pnm format.
+    def save_image(self, pic):
+        return None
