0fb675a85f9fd5d1fce66ff470c2f020e0add80a
Evan Sobkowicz <evansobkowicz@gmail.com>
2015-02-26 09:27:39 -0500

    working on reading pnmm file

--- main.py
+++ main.py
@@ -7,10 +7,27 @@ class Edger:
     # Initialize Object
     def __init__(self, file_path):
         self.file_path = file_path
-
+        self.create_image()
 
     # Read in pnm P2 or P3 files from text
-    def create_image(self, file_path):
+    def create_image(self):
+        path = "img/" + self.file_path
+        f = open(path, "r")
+        img_type = f.readline()
+        img_size = f.readline()
+        sizes = img_size.split()
+        width = int(sizes[0])
+        height = int(sizes[1])
+        data = list()
+        lines = f.readlines()
+        for line in lines:
+            clean_line = line.strip('\n').strip(' ')
+            data.append(clean_line)
+        f.close()
+        print 'Type:', img_type
+        print 'Size:', sizes
+        print 'Length', len(data), ' = ', int(sizes[0])*int(sizes[1])
+        print data
         return None
 
     # Grayscale
@@ -31,3 +48,7 @@ class Edger:
     # Save your edge image to a file using the greyscale .pnm format.
     def save_image(self, pic):
         return None
+
+
+
+e = Edger('rainbow.pnm')
