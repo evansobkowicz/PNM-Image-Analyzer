e9e83af5bb9b0719d9f194876461dc96c77b82c3
Evan Sobkowicz <evansobkowicz@gmail.com>
2015-02-26 18:40:59 -0500

    working on images

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 # Project 1
 # Evan Sobkowicz
 
+from graphics import *
+
 
 class Edger:
 
     # Initialize Object
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self, file):
+        self.file_path = file
+        self.height = 0
+        self.width = 0
         self.img_data = list()
+        self.img = None
 
         # Run the Program
+        self.read_file()
         self.create_image()
-
+        self.display_image()
 
     # HELPER METHODS
-
     # Image Size Parser
     def img_size(self, data):
         sizes = data.split()
@@ -25,20 +30,31 @@ class Edger:
         return s.strip('\n').strip(' ')
 
     # Read in pnm P2 or P3 files from text
-    def create_image(self):
+    def read_file(self):
+        print 'Loading File...'
         path = "img/" + self.file_path
         f = open(path, "r")
         img_type = self.cleaner(f.readline())
+        data = list()
         if img_type == 'P2' or img_type == 'P3':
-            width, height = self.img_size(f.readline())
-            data = list()
-            for i in range(width * height):
+            self.width, self.height = self.img_size(f.readline())
+            for i in range(self.width * self.height):
                 clean_line = self.cleaner(f.readline())
                 data.append(clean_line)
         else:
             print 'ERROR: Invalid File Type.'
         f.close()
         self.img_data = data
+        print 'Finished Loading File...'
+
+
+    # Create Image
+    def create_image(self):
+        self.img = Image(Point(self.width/2, self.height/2), self.width, self.height)
+        for x in range(self.width * self.height):
+            # for y in range(self.height):
+            color = int(self.img_data[x])
+            self.img.setPixel(x, x, color_rgb(color, color, color))
 
     # Grayscale
     def grayscale(self, pic):
@@ -52,8 +68,11 @@ class Edger:
 
 
     # Display edged image
-    def display_image(self, pic):
-        return None
+    def display_image(self):
+        win = GraphWin('Image', self.width, self.height)
+        self.img.draw(win)
+        win.getMouse()
+        win.close()
 
 
     # Save your edge image to a file using the greyscale .pnm format.
