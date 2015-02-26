2b82937f17fa95a7a65d87c2d0131d1a06e3dce7
Evan Sobkowicz <evansobkowicz@gmail.com>
2015-02-26 09:44:20 -0500

    image loading funcitonal

--- main.py
+++ main.py
@@ -7,28 +7,38 @@ class Edger:
     # Initialize Object
     def __init__(self, file_path):
         self.file_path = file_path
+        self.img_data = list()
+
+        # Run the Program
         self.create_image()
+
+
+    # HELPER METHODS
+
+    # Image Size Parser
+    def img_size(self, data):
+        sizes = data.split()
+        return int(sizes[0]), int(sizes[1])
+
+    # String Cleaner - Strip new line and white space
+    def cleaner(self, s):
+        return s.strip('\n').strip(' ')
 
     # Read in pnm P2 or P3 files from text
     def create_image(self):
         path = "img/" + self.file_path
         f = open(path, "r")
-        img_type = f.readline()
-        img_size = f.readline()
-        sizes = img_size.split()
-        width = int(sizes[0])
-        height = int(sizes[1])
-        data = list()
-        lines = f.readlines()
-        for line in lines:
-            clean_line = line.strip('\n').strip(' ')
-            data.append(clean_line)
+        img_type = self.cleaner(f.readline())
+        if img_type == 'P2' or img_type == 'P3':
+            width, height = self.img_size(f.readline())
+            data = list()
+            for i in range(width * height):
+                clean_line = self.cleaner(f.readline())
+                data.append(clean_line)
+        else:
+            print 'ERROR: Invalid File Type.'
         f.close()
-        print 'Type:', img_type
-        print 'Size:', sizes
-        print 'Length', len(data), ' = ', int(sizes[0])*int(sizes[1])
-        print data
-        return None
+        self.img_data = data
 
     # Grayscale
     def grayscale(self, pic):
